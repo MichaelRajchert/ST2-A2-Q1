8da0a6459a79d2462b114633f1042905e3406fdc
MichaelRajchert <mikerajchert@gmail.com>
2017-04-28 20:32:49 +1000

    Added new .java files

--- src/main.java
+++ src/main.java
@@ -1,24 +1,15 @@
 import java.util.Stack;
 
 /**
- * Created by MichaelRajchert on 28/04/2017.
+ * Created by MichaelRajchert on 28/04/2017
+ *
+ * For ST2-ASSIGNMENT1-Q1
  */
 public class main {
-    public static Function_class function = new Function_class();
-    public static Stack stack = new Stack();
+    public static StackOperations stackOperations = new StackOperations();
+
+
     public static void main(String[] args){
-
-    }
-}
-
-class Function_class{
-    public static void push(){
-
-    }
-    public static void pop(){
-
-    }
-    public static void peek(){
 
     }
 }
