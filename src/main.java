2ef8692a8b1f47707a5a2f9dc1996fc20bbed856
MichaelRajchert <mikerajchert@gmail.com>
2017-04-28 12:00:43 +1000

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+/**
+ * Created by MichaelRajchert on 28/04/2017.
+ */
+public class main {
+    public static Function_class function = new Function_class();
+    public static Stack stack = new Stack();
+    public static void main(String[] args){
+
+    }
+}
+
+class Function_class{
+    public static void push(){
+
+    }
+    public static void pop(){
+
+    }
+    public static void peek(){
+
+    }
+}
